d9e1b3e62f1ea67f665edb2e30dfb07ffb775af1
Amilton Machado <amilton.machado@fw7.com.br>
2021-04-19 21:25:49 -0300

    Version: 1.0.1.0 (202104192125) / include organized

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,24 @@ const { connect } = require('./../inc/db');
 var router = express.Router();
 
 /**
- * Constants default.
+ * Constants default. { Devem buscar as informações do banco de dados.}
  */
 const titleProject = 'AMRestaurant';
 
-/* GET home page. */
+/**
+ * Images background.
+ */
+
+const imgPath = 'images/backgrounds/';
+const imgHeaderIndex = 'index.jpg'; 
+const imgHeaderContacts = 'contacts.jpg';
+const imgHeaderMenus = 'menus.jpg'; 
+const imgHeaderReservations = 'reservations.jpg'; 
+const imgHeaderServices = 'services.jpg'; 
+
+/**
+ *  --- Index ------------------------------------------------------------------
+ */
 router.get('/', function(req, res, next) {
   
   connection.query(`SELECT id, title, description, price, photo, register 
@@ -20,40 +33,61 @@ router.get('/', function(req, res, next) {
                       
                       res.render('index', { 
                         title: titleProject,
+                        imgBackground: imgPath + imgHeaderIndex,
                         menus: results
                       });
                     }
                   );
 });
 
+/**
+ *  --- Contacts ---------------------------------------------------------------
+ */
 router.get('/contacts', function(req, res, next){
   
   res.render('contacts', {
     title: titleProject,
+    subTitle: 'Sub-título',
+    imgBackground: imgPath + imgHeaderContacts,
   })
   
 })
 
+/**
+ *  --- Menus ------------------------------------------------------------------
+ */
 router.get('/menus', function(req, res, next){
   
   res.render('menus', {
     title: titleProject,
+    subTitle: 'Sub-título',
+    imgBackground: imgPath + imgHeaderMenus,
   })
 
 })
 
+/**
+ *  --- Reservations ----------------------------------------------------------- 
+ */
 router.get('/reservations', function(req, res, next){
   
   res.render('reservations', {
     title: titleProject,
+    subTitle: 'Sub-título',
+    imgBackground: imgPath + imgHeaderReservations,
   })
 
 })
 
+/**
+ *  --- Services --------------------------------------------------------------- 
+ */
 router.get('/services', function(req, res, next){
   
   res.render('services', {
     title: titleProject,
+    subTitle: 'Sub-título',
+    imgBackground: imgPath + imgHeaderServices,
   })
 
 })
